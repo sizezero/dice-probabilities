3b873ed9a5b1bf8a868ce27c6243b05b6f10f004
Robert Kleemann <robert@kleemann.org>
2014-01-05 19:11:13 -0800

    added bounds test

--- src/org/kleemann/diceprobabilities/GraphView.java
+++ src/org/kleemann/diceprobabilities/GraphView.java
@@ -30,12 +30,18 @@ public class GraphView extends View {
 	 */
 	@Override
 	protected void onDraw(Canvas canvas) {
-		final int h = canvas.getHeight();
-		final int w = canvas.getWidth();
-		
+		bounds(canvas);
+	}
+
+	private void blueBackground(Canvas canvas) {
 		Paint p = new Paint();
 		p.setARGB(255, 0, 0, 255); // blue; full alpha		
-		canvas.drawPaint(p);
+		canvas.drawPaint(p);		
+	}
+	
+	private void curvy(Canvas canvas) {
+		final int h = canvas.getHeight();
+		final int w = canvas.getWidth();
 		
 		// break the drawable image into 0-1, 0-1
 		final float[] points = {
@@ -48,18 +54,58 @@ public class GraphView extends View {
 		                        0.7f, 0.9f,
 		                        0.8f, 1.0f,
 		                        0.9f, 1.0f,
-		                        1.0f, 1.0f
+		                        0.99f, 0.99f
 		};
+		float[] mid = new float[points.length-2];
+		for (int i=0 ; i<mid.length ; ++i) {
+			mid[i] = (points[i]+points[i+2]) / 2.0f;
+		}
 		
 		// for now construct a path that moves through mid-points
 		Path pa = new Path();
+        
+		pa.moveTo(points[0]*w, points[1]*h);
+		for (int i=0 ; i<mid.length ; i += 2) {
+			pa.quadTo(points[i]*w,points[i+1]*h, mid[i]*w,mid[i+1]*h);
+		}
+		//pa.lineTo(w,h);
+
+		blueBackground(canvas);
+		
+		Paint p = new Paint();
 		p.setStyle(Paint.Style.STROKE);
         p.setStrokeWidth(5f);
         p.setColor(Color.WHITE);
-		pa.moveTo(0, 0);
-		pa.lineTo(w,h);
-		canvas.drawPath(pa, p);
+		canvas.drawPath(pa, p);		
 	}
 
-	
+	private void bounds(Canvas canvas) {
+		final int h = canvas.getHeight();
+		final int w = canvas.getWidth();
+		
+		Paint p = new Paint();
+		p.setStyle(Paint.Style.STROKE);
+        p.setStrokeWidth(0f);
+
+        // blue background
+        blueBackground(canvas);
+        
+        // draw a bounding box in white
+        p.setColor(Color.RED);
+		canvas.drawLine(0, 0, w-1, 0, p);		
+		canvas.drawLine(0, h-1, w-1, h-1, p);		
+		canvas.drawLine(0, 0, 0, h-1, p);		
+		canvas.drawLine(w-1, 0, w-1, h-1, p);
+		
+		// The above bounding box lacks the bottom horz line in the emulator
+		// on an actual nexus 5 it lacks the top horz line and the right vert line
+		
+		// diagonal
+		p.setColor(Color.YELLOW);
+		canvas.drawLine(0,0,w-1,h-1,p);
+		canvas.drawLine(0,h-1,w-1,0,p);
+		
+		// emulator appears to be truncated to some degree (~10%)
+		// due to small screen resolution
+	}
 }
