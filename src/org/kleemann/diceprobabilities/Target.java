e363bc5a053172bdf6a50019fd3428c02aa121b4
Robert Kleemann <robert@kleemann.org>
2014-02-10 20:21:17 -0800

    pressing target cycles between large values

--- src/org/kleemann/diceprobabilities/Target.java
+++ src/org/kleemann/diceprobabilities/Target.java
@@ -4,21 +4,23 @@ import android.view.View;
 import android.widget.Button;
 
 /**
- * <p>A special type of CurrentDicePile that represents the integer 
- * target of the roll.  Primary difference is the rendering of the value
- * and a different default value.
+ * <p>
+ * A special type of CurrentDicePile that represents the integer target of the
+ * roll. Primary difference is the rendering of the value and a different
+ * default value.
  */
 public class Target implements View.OnClickListener {
 
-	private static final String GREATER_THAN_OR_EQUAL_TO = "\u2265"; 
-	
+	private static final String GREATER_THAN_OR_EQUAL_TO = "\u2265";
+
 	private final int defaultTarget;
 	private int count;
 	private Button button;
 	private View.OnClickListener changed;
-	
+
 	public Target(Button button, View.OnClickListener changed) {
-		this.defaultTarget = button.getResources().getInteger(R.integer.default_target);
+		this.defaultTarget = button.getResources().getInteger(
+				R.integer.default_target);
 		this.count = defaultTarget;
 		this.button = button;
 		this.changed = changed;
@@ -29,25 +31,36 @@ public class Target implements View.OnClickListener {
 	private void updateButton() {
 		button.setText(GREATER_THAN_OR_EQUAL_TO + count);
 	}
-	
-	public int getCount() { return count; }
-	
+
+	public int getCount() {
+		return count;
+	}
+
 	public void setCount(int count) {
-		assert(count >= 0);
+		assert (count >= 0);
 		this.count = count;
 		updateButton();
-		changed.onClick(button);		
+		changed.onClick(button);
 	}
-	
+
 	public void add(int count) {
 		int n = this.count + count;
-		setCount(n<0 ? 0 : n);
+		setCount(n < 0 ? 0 : n);
 	}
 
 	public void clear() {
 		setCount(defaultTarget);
 	}
-	
+
+	/**
+	 * Cycle between 0, 10, 20, 30
+	 */
 	@Override
-	public void onClick(View v) { add(-1); }
+	public void onClick(View v) {
+		int n = (((count + 10) / 10) * 10);
+		if (n > 30) {
+			n = 0;
+		}
+		setCount(n);
+	}
 }
