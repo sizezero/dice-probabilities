0d15f1b7c25668aeb64583e793793bcc9c8b8b76
Robert Kleemann <robert@kleemann.org>
2014-02-12 17:15:24 -0800

    updated javadocs

--- src/org/kleemann/diceprobabilities/distribution/Distribution.java
+++ src/org/kleemann/diceprobabilities/distribution/Distribution.java
@@ -4,12 +4,31 @@ import org.apache.commons.math3.fraction.BigFraction;
 
 /**
  * <p>
- * Represents a discrete distribution that exists of the integer set 0 to
- * size()-1 Each distribution is represented as a fraction and the sum of all
- * the fractions must equal 1.
+ * Represents a discrete distribution that exists over the integer set 0 to
+ * size()-1 A distribution consists of discrete events (x values) that have a
+ * probabilty (returned by getProbability(y)) that each have an independent
+ * probability. Independence means:
  * 
  * <p>
- * All our implementation of Distribution are immutable.
+ * <ol>
+ * 
+ * <li>only one event can happen.
+ * 
+ * <li>one event is guaranteed to happen.
+ * 
+ * </ol>
+ * 
+ * <p>
+ * A side effect of this is that all probabilities in a distribution must add up
+ * to 100% (one)
+ * 
+ * <p>
+ * In addition to the probability of event x it also can be useful to look at the
+ * Cumulative probability which is the probability that an event greater than or
+ * equal to x occurs. This is returned by getCumulativeProbability(x)
+ * 
+ * <p>
+ * All our implementations of Distribution are immutable and thus thread-safe
  */
 public interface Distribution {
 
@@ -24,12 +43,16 @@ public interface Distribution {
 
 	/**
 	 * <p>
-	 * Returns the probability of the random variable at the point x. There is
-	 * no range for x so "out of bounds" values return a probability of zero.
+	 * Returns the probability of the event at the point x. There is no range
+	 * for x so "out of bounds" values return a probability of zero.
 	 * 
 	 * <p>
 	 * Note: x values in this application generally represent sums of dice
 	 * rolls.
+	 * 
+	 * <p>
+	 * For all implementations of Distribution this method runs in constant
+	 * time; O(1)
 	 */
 	BigFraction getProbability(int x);
 
@@ -37,6 +60,13 @@ public interface Distribution {
 	 * <p>
 	 * Returns the sum of all probabilities in this distribution from point x
 	 * and greater.
+	 * 
+	 * <p>
+	 * This usually indicates succeeding at a roll where the target is x.
+	 * 
+	 * <p>
+	 * Some implementations run in constant time some in linear time. If many
+	 * calls to this function are made, call cacheCumulative()
 	 */
 	BigFraction getCumulativeProbability(int x);
 
