c51e7ef8e71e98feaaaecd3fb12cda160a92fe2a
Robert Kleemann <robert@kleemann.org>
2014-08-14 18:22:21 -0700

    added extra test for Death Zone

--- tests/src/org/kleemann/diceprobabilities/distribution/DeathZoneTest.java
+++ tests/src/org/kleemann/diceprobabilities/distribution/DeathZoneTest.java
@@ -10,13 +10,28 @@ public class DeathZoneTest extends TestCase {
 		Distribution d4 = new DeathZoneDieDistribution(4);
 		assertEquals(BigFraction.ZERO, d4.getProbability(-2));
 		assertEquals(BigFraction.ZERO, d4.getProbability(-1));
-		assertEquals(new BigFraction(1, 2), d4.getProbability(0));
+		assertEquals(new BigFraction(2, 4), d4.getProbability(0));
 		assertEquals(BigFraction.ZERO, d4.getProbability(1));
 		assertEquals(BigFraction.ZERO, d4.getProbability(2));
 		assertEquals(new BigFraction(1, 4), d4.getProbability(3));
 		assertEquals(new BigFraction(1, 4), d4.getProbability(4));
 		assertEquals(BigFraction.ZERO, d4.getProbability(5));
 		assertEquals(BigFraction.ZERO, d4.getProbability(6));
+	}
+
+	public void testSix() {
+		Distribution d6 = new DeathZoneDieDistribution(6);
+		assertEquals(BigFraction.ZERO, d6.getProbability(-2));
+		assertEquals(BigFraction.ZERO, d6.getProbability(-1));
+		assertEquals(new BigFraction(2, 6), d6.getProbability(0));
+		assertEquals(BigFraction.ZERO, d6.getProbability(1));
+		assertEquals(BigFraction.ZERO, d6.getProbability(2));
+		assertEquals(new BigFraction(1, 6), d6.getProbability(3));
+		assertEquals(new BigFraction(1, 6), d6.getProbability(4));
+		assertEquals(new BigFraction(1, 6), d6.getProbability(5));
+		assertEquals(new BigFraction(1, 6), d6.getProbability(6));
+		assertEquals(BigFraction.ZERO, d6.getProbability(7));
+		assertEquals(BigFraction.ZERO, d6.getProbability(8));
 	}
 
 }
